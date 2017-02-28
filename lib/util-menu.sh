a077d7362eacd69db323e1f72fa80c09ba5c728f
Bernhard Landauer <oberon@manjaro.org>
2017-03-01 00:46:15 +0100

    new main_menu()

--- lib/util-menu.sh
+++ lib/util-menu.sh
@@ -1,54 +1,55 @@
-main_menu_online() {
-    if [[ $HIGHLIGHT != 9 ]]; then
-       HIGHLIGHT=$(( HIGHLIGHT + 1 ))
-    fi
+main_menu() {
+    declare -i loopmenu=1
+    while ((loopmenu)); do
+        if [[ $HIGHLIGHT != 9 ]]; then
+           HIGHLIGHT=$(( HIGHLIGHT + 1 ))
+        fi
 
-    DIALOG " $_MMTitle " --default-item ${HIGHLIGHT} \
-      --menu "$_MMBody" 0 0 9 \
-      "1" "$_PrepMenuTitle|>" \
-      "2" "$_InstBsMenuTitle|>" \
-      "3" "$_InstGrMenuTitle|>" \
-      "4" "$_ConfBseMenuTitle|>" \
-      "5" "$_InstNMMenuTitle|>" \
-      "6" "$_InstMultMenuTitle|>" \
-      "7" "$_SecMenuTitle|>" \
-      "8" "$_SeeConfOptTitle|>" \
-      "9" "$_Done" 2>${ANSWER}
+        DIALOG " $_MMTitle " --default-item ${HIGHLIGHT} \
+          --menu "$_MMBody" 0 0 9 \
+          "1" "$_PrepMenuTitle|>" \
+          "2" "$_InstBsMenuTitle|>" \
+          "3" "$_ConfBseMenuTitle|>" \
+          "4" "$_InstGrMenuTitle|>" \
+          "5" "$_InstNMMenuTitle|>" \
+          "6" "$_InstMultMenuTitle|>" \
+          "7" "$_SecMenuTitle|>" \
+          "8" "$_SeeConfOptTitle|>" \
+          "9" "$_Done" 2>${ANSWER}
 
-    HIGHLIGHT=$(cat ${ANSWER})
+        HIGHLIGHT=$(cat ${ANSWER})
 
-    # Depending on the answer, first check whether partition(s) are mounted and whether base has been installed
-    if [[ $(cat ${ANSWER}) -eq 2 ]]; then
-       check_mount
-    fi
+        # Depending on the answer, first check whether partition(s) are mounted and whether base has been installed
+        if [[ $(cat ${ANSWER}) -eq 2 ]]; then
+           check_mount
+        fi
 
-    if [[ $(cat ${ANSWER}) -ge 3 ]] && [[ $(cat ${ANSWER}) -le 8 ]]; then
-       check_mount
-       check_base
-    fi
+        if [[ $(cat ${ANSWER}) -ge 3 ]] && [[ $(cat ${ANSWER}) -le 8 ]]; then
+           check_mount
+           check_base
+        fi
 
-    case $(cat ${ANSWER}) in
-        "1") prep_menu
-            ;;
-        "2") install_base_menu
-            ;;
-        "3") install_graphics_menu
-            ;;
-        "4") config_base_menu
-            ;;
-        "5") install_network_menu
-            ;;
-        "6") install_multimedia_menu
-            ;;
-        "7") security_menu
-            ;;
-        "8") edit_configs
-            ;;
-        *) exit_done
-            ;;
-    esac
-
-    main_menu_online
+        case $(cat ${ANSWER}) in
+            "1") prep_menu
+                ;;
+            "2") install_base_menu
+                ;;
+            "3") config_base_menu
+                ;;
+            "4") install_graphics_menu
+                ;;
+            "5") install_network_menu
+                ;;
+            "6") install_multimedia_menu
+                ;;
+            "7") security_menu
+                ;;
+            "8") edit_configs
+                ;;
+            *) exit_done
+                ;;
+        esac
+    done
 }
 
 # Preparation
