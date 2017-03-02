400bb5f33eb33663c2a0f6c03ddb5e3179cb2796
Bernhard Landauer <oberon@manjaro.org>
2017-03-02 20:56:27 +0100

    add comment about db update to intro

--- lib/util.sh
+++ lib/util.sh
@@ -286,7 +286,7 @@ check_requirements() {
 
     # This will only be executed where neither of the above checks are true.
     # The error log is also cleared, just in case something is there from a previous use of the installer.
-    DIALOG " $_ReqMetTitle " --infobox "$_ReqMetBody" 0 0
+    DIALOG "\n $_ReqMetTitle $_UpdDb\n\n" --infobox "$_ReqMetBody" 0 0
     sleep 2
     clear
     echo "" > $ERR
